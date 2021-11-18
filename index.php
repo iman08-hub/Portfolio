64751fb9ab9601aac259618061105bc5a58201d8
iman08-hub <81545845+iman08-hub@users.noreply.github.com>
2021-11-18 13:09:47 +0600

    Update index.php

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ span.psw {
 
 <h2>Login Form</h2>
 
-<form action="portfolio.php" method="post">
+<form action="Portfolio.php" method="post">
   <div class="imgcontainer">
     <img src="Ичиго.jpeg.png" alt="Avatar" class="avatar">
   </div>
